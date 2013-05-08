2d57b2e10e153a75a89bf5dc8e68e02fbff85d10
Artyom Semyonov <sevenov@gmail.com>
2013-05-09 00:14:27 +0400

    Just reimplementing part of code for working with VIM

--- Classes/App.h
+++ Classes/App.h
@@ -1,13 +1,6 @@
 #import <Cocoa/Cocoa.h>
 
 @interface App : NSObject {
-    NSString *path;
-
-    IBOutlet NSWindow *prefPanel;
-    IBOutlet NSTextField *textField;
 }
-
--(IBAction)showPrefPanel:(id)sender;
--(IBAction)applyChange:(id)sender;
 
 @end
--- Classes/App.m
+++ Classes/App.m
@@ -4,20 +4,17 @@
 
 @implementation App
 
-NSString *defaultPath = @"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl";
-
 -(void)awakeFromNib {
-    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
-    path = [d objectForKey:@"path"];
-
     NSAppleEventManager *appleEventManager = [NSAppleEventManager sharedAppleEventManager];
-    [appleEventManager setEventHandler:self andSelector:@selector(handleGetURLEvent:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
+    [appleEventManager
+        setEventHandler:self
+        andSelector:@selector(handleGetURLEvent:withReplyEvent:)
+        forEventClass:kInternetEventClass
+        andEventID:kAEGetURL
+    ];
 }
 
 -(void)handleGetURLEvent:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent {
-    if (nil == path) path = defaultPath;
-
-    // txmt://open/?url=file://~/.bash_profile&line=11&column=2
     NSURL *url = [NSURL URLWithString:[[event paramDescriptorForKeyword:keyDirectObject] stringValue]];
 
     if (url && [[url host] isEqualToString:@"open"]) {
@@ -26,42 +23,17 @@ NSString *defaultPath = @"/Applications/Sublime Text 2.app/Contents/SharedSuppor
 
         if (file_url && [[file_url scheme] isEqualToString:@"file"]) {
             NSString *file = [file_url path];
-            NSString *line = [params objectForKey:@"line"];
-            NSString *column = [params objectForKey:@"column"];
 
             if (file) {
                 NSTask *task = [[NSTask alloc] init];
-                [task setLaunchPath:path];
-                [task setArguments:[NSArray arrayWithObjects: [NSString stringWithFormat:@"%@:%lu:%lu", file, [line integerValue], [column integerValue]], nil]];
+                [task setLaunchPath: @"/usr/bin/open"];
+                [task setArguments:[NSArray arrayWithObjects: @"-a", @"MacVIM", file, nil]];
+                
                 [task launch];
                 [task release];
             }
         }
     }
-
-    if (![prefPanel isVisible]) {
-        [NSApp terminate:self];
-    }
-}
-
--(IBAction)showPrefPanel:(id)sender {
-    if (path) {
-        [textField setStringValue:path];
-    } else {
-        [textField setStringValue:defaultPath];
-    }
-    [prefPanel makeKeyAndOrderFront:nil];
-}
-
--(IBAction)applyChange:(id)sender {
-    path = [textField stringValue];
-
-    if (path) {
-        NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
-        [d setObject:path forKey:@"path"];
-    }
-
-    [prefPanel orderOut:nil];
 }
 
 @end
